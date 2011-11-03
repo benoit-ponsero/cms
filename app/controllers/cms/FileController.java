b22a1e90568da52bb8e1ec8fde81295d51ddb9b1
zk <b.ponsero@gmail.com>
2011-11-03 10:31:36 +0100

    move /public/files outside application

---
+++ app/controllers/cms/FileController.java
@@ -0,0 +1,30 @@
+package controllers.cms;
+
+import java.io.File;
+import play.Play;
+import play.mvc.Controller;
+import plugins.router.Route;
+
+/**
+ * @author benoit
+ */
+public class FileController extends Controller {
+    
+    @Route("/public/files/{filepath}")
+    public static void files(String filepath) {
+        
+        String rootPath = Play.applicationPath.getAbsolutePath();
+        rootPath = rootPath.substring(0, rootPath.lastIndexOf("/"));
+        rootPath += "/__files/" + Play.configuration.getProperty("application.name");
+        
+        File file = new File(rootPath, filepath);
+        
+        if (file.exists()){
+            renderBinary(file);
+        }
+        else {
+            
+            error(404, "File not Found");
+        }
+    }
+}
