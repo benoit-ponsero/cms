f69a545907bba3af5cf30118d9e9c4ed3d09cac7
zk <b.ponsero@gmail.com>
2011-11-03 11:54:16 +0100

    add table name for virtualpagetemplate

--- app/models/cms/VirtualPageTemplate.java
+++ app/models/cms/VirtualPageTemplate.java
@@ -1,12 +1,14 @@
 package models.cms;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import play.db.jpa.Model;
 
 /**
  * @author benoit
  */
 @Entity
+@Table(name="cms_virtual_page_template")
 public class VirtualPageTemplate extends Model{
     
     public String name;
